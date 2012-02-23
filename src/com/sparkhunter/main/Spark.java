718c901cb70e762746cd2d88c274237d83075752
Travis Porter <travis.porter@gmail.com>
2012-02-22 22:14:28 -0500

    Added Spark class

---
+++ src/com/sparkhunter/main/Spark.java
@@ -0,0 +1,15 @@
+package com.sparkhunter.main;
+
+public class Spark {
+	//stats
+	private int mMaxHP = 100; //100 default cause why not
+	private int mCurHP = mMaxHP;
+	
+	private String mName;
+	
+	public Spark(String name)
+	{
+		mName = name;
+		
+	}
+}

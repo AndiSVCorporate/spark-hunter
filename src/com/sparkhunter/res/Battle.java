b9525c5de1f8ec6574cedaa6620b52ad8e01bd1b
hakujin <newtonj101@gmail.com>
2012-03-13 20:08:54 -0400

    re-added refactored classes

---
+++ src/com/sparkhunter/res/Battle.java
@@ -0,0 +1,20 @@
+package com.sparkhunter.res;
+
+
+public class Battle {
+	public Spark mYourSpark;
+	public Spark mHisSpark;
+	
+	public Battle(Spark your, Spark his)
+	{
+		mYourSpark = your;
+		mHisSpark = his;
+		
+	}
+	public String attack()
+	{
+		
+		return null;
+		
+	}
+}
---
+++ src/com/sparkhunter/res/Inventory.java
@@ -0,0 +1,48 @@
+package com.sparkhunter.res;
+
+import java.util.Vector;
+import android.util.Log;
+
+//TODO make static and connect to master game state
+//TODO verify that state changes in InventoryScreen don't mess with this
+public class Inventory {
+	//TODO loads/store inventory locally
+	private Vector<Item> inventoryItems = new Vector<Item>(); // actual item list
+	
+	public Inventory(){
+		//the player only has rubber ducks for now, dealwithit.jpg
+		for(int i = 0; i < 6; i++){
+			inventoryItems.add(new RubberDuck());
+		}
+	}
+	
+	public Vector<Item> getItemList(){
+		return inventoryItems;
+	}
+	
+	public void addItem(Item newItem){
+		inventoryItems.add(newItem);
+		return;
+	}
+	
+	public boolean removeItem(int itemID){
+		int i = 0;
+		boolean found = false;
+		
+		//scan the inventory for the matching ID, then delete it.
+		try{
+			while(!found){
+				if(inventoryItems.elementAt(i).getIdentifier() == itemID){
+					inventoryItems.remove(i);
+					found = true;
+				}
+			}
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			//catch when someone tries to delete an item that doesn't exist.
+			Log.d("INVENTORY", "ITEM NOT FOUND");
+		}
+		
+		return found;
+	}
+}

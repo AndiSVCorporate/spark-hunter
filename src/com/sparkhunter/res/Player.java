22163df7470117044d8d0eca8707510e75576d4d
hakujin <newtonj101@gmail.com>
2012-03-15 10:53:37 -0400

    Inventories are initialized in Player class.

--- src/com/sparkhunter/res/Player.java
+++ src/com/sparkhunter/res/Player.java
@@ -4,8 +4,8 @@ public class Player {
 	//critical to make player a singleton, for cross-activity usage
 	private static Player singletonPlayer = new Player();
 	
-	private Inventory itemInventory;
-	private Inventory sparkInventory;
+	private Inventory itemInventory = new Inventory();
+	private Inventory sparkInventory = new Inventory();
 	
 	private Player() {}
 	

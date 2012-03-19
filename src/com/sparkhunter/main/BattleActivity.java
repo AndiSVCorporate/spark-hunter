0d0c70ee94c514d62f4203bcd70f1b82348ff9b6
Travis Porter <travis.porter@gmail.com>
2012-03-19 00:51:34 -0400

    Implemented inventory and run away buttons in battle.

--- src/com/sparkhunter/main/BattleActivity.java
+++ src/com/sparkhunter/main/BattleActivity.java
@@ -2,9 +2,11 @@ package com.sparkhunter.main;
 
 import com.sparkhunter.res.Ability;
 import com.sparkhunter.res.Battle;
+import com.sparkhunter.res.Map;
 import com.sparkhunter.res.Spark;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -23,8 +25,11 @@ public class BattleActivity extends Activity{
 	private Battle mBattle;
 	ProgressBar mLeftBar;
     ProgressBar mRightBar;
-    static MediaPlayer music;
+    
+    private boolean mEnd = false;
+    static MediaPlayer bgm;
     static TextView mBattleLog;
+    
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		mActivity = this;
@@ -38,8 +43,8 @@ public class BattleActivity extends Activity{
         
         
         //BATTLE MOOSIC
-        music = MediaPlayer.create(mActivity, R.raw.mlp_rainbowdash);
-        music.start();
+        bgm = MediaPlayer.create(mActivity, R.raw.mlp_rainbowdash);
+        bgm.start();
         
         mBattle = new Battle(GetSpark.chosenSpark,new Spark("Poke-man",R.drawable.item_diamond));
         
@@ -60,11 +65,41 @@ public class BattleActivity extends Activity{
         print("BATTLE START");
         
         initializeAttackMenu();
+        Button b = (Button)findViewById(R.id.battleInventory);
         
+        
+        
+        
+        
+        
+        b.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				bgm.stop();
+				Intent i = new Intent(mActivity, InventoryScreen.class);
+				startActivity(i);
+			}
+		});
+        b = (Button)findViewById(R.id.battleRun);
+        b.setOnClickListener(new View.OnClickListener() {
+        	
+			public void onClick(View v) {
+				if(mBattle.run()){
+					print("Got away succesfully.");
+					endBattle();
+				}
+				else{
+					print("Run failed.");
+					print(mBattle.attack("Poke",mBattle.mHisSpark,mBattle.mYourSpark, false));
+					endBattle();
+				}
+			}
+			
+		});
         refresh();
 	}
 	private void initializeAttackMenu(){
-        Spinner s = (Spinner) findViewById(R.id.attack);
+        Spinner s = (Spinner) findViewById(R.id.battleAttack);
         ArrayAdapter<CharSequence> adapter;
         adapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, Ability.extractChar(mBattle.mYourSpark.getAbilities()));
        s.setOnItemSelectedListener(new OnItemSelectedListener() {
@@ -94,32 +129,49 @@ public class BattleActivity extends Activity{
 
 	@Override
 	public void onPause(){
-		music.pause();
+		//bgm.prepare();
+		//bgm.pause();
 		super.onPause();
 	}
 	
 	@Override
 	public void onStop(){
-		music.release();
+		//bgm.release();
 		super.onStop();
 	}
 	@Override	
 	public void onResume(){
-		music.start();
 		super.onResume();
+		//bgm.start();
+	}
+	@Override
+	public void onBackPressed() {
+		   return;
 	}
 	public void refresh(){
 		mLeftBar.setProgress(mBattle.mYourSpark.mCurHP);
 		mRightBar.setProgress(mBattle.mHisSpark.mCurHP);
 		if(mBattle.mHisSpark.mCurHP<=0){
 			print(mBattle.setWin());
-			Spinner s = (Spinner) findViewById(R.id.attack);
-			s.setClickable(false);
+			endBattle();
 		}
-		if(mBattle.mYourSpark.mCurHP<=0){
+		else if(mBattle.mYourSpark.mCurHP<=0){
 			print(mBattle.setLose());
-			Spinner s = (Spinner) findViewById(R.id.attack);
-			s.setClickable(false);
+			endBattle();
 		}
+	}
+	public void endBattle(){
+		Spinner s = (Spinner) findViewById(R.id.battleAttack);
+		s.setClickable(false);
+
+        Button b = (Button)findViewById(R.id.battleRun);
+        b.setText("Leave Battle");
+        b.setOnClickListener(new View.OnClickListener() {
+        	
+			public void onClick(View v) {
+				mActivity.finish();
+			}
+			
+		});
 	}
 }

39ff7e37004a3a44d885f7ea15514a760bbc0fe7
unknown <Divyang@Divyang-PC.(none)>
2012-04-10 18:26:04 -0400

    Recreated the battle screen to be usable on differentdevices without distortion.

--- src/com/sparkhunter/res/AdventureService.java
+++ src/com/sparkhunter/res/AdventureService.java
@@ -126,7 +126,7 @@ public class AdventureService extends Service {
 				Context context = getApplicationContext();
 				CharSequence content = "Spark Hunter";
 				CharSequence text = "You have been attacked";
-				Intent noteIntent = new Intent(AdventureService.this, AdventureTime.class);
+				Intent noteIntent = new Intent(AdventureService.this, com.sparkhunter.activities.AdventureTime.class);
 				PendingIntent contentIntent = PendingIntent.getActivity(AdventureService.this,0,noteIntent,0);
 				note.setLatestEventInfo(context, content, text, contentIntent);
 				

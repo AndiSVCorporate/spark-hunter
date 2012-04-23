423846bfd9df9685311eacb5babf47e1eccbad93
hakujin <newtonj101@gmail.com>
2012-04-23 19:29:55 -0400

    Abstracted SQL reads into GameDataManager.

--- src/com/sparkhunter/res/GameDataManager.java
+++ src/com/sparkhunter/res/GameDataManager.java
@@ -1,17 +1,84 @@
 package com.sparkhunter.res;
 
+import java.util.Vector;
+
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.util.Log;
 
 public class GameDataManager {
+	private Context currentContext;
 	private SQLiteDatabase database;
 	private SQLGameDataOpener databaseOpener;
     private static final String DATABASE_NAME = "spark_hunter.db";
     private static final String[] DATABASE_RES = {"spark_hunter", "/raw", "com.sparkhunter.main:"};
     private static final String DATABASE_PATH = "/data/data/com.sparkhunter.main/databases/";
+	private static final String PLAYER_TABLE = "player_data";
 	
 	public GameDataManager(Context c){
 		databaseOpener = new SQLGameDataOpener(c);
+		currentContext = c;
+		
+		//use it get a handle on the main database
+		database = databaseOpener.open();
+	}
+	
+	//returns player's item inventory
+	
+	//split off some helper methods from this
+	public Inventory getAllPlayerItems(){
+		Cursor query = generateQuery(PLAYER_TABLE, "TYPE == ITEM");
+		
+		return readInventory(query);
+	}
+	
+	public Inventory getAllPlayerSparks(){
+		Cursor query = generateQuery(PLAYER_TABLE, "TYPE == SPARK");
+		
+		return readInventory(query);
+	}
+	
+	private Cursor generateQuery(String table, String where){
+		//read in the data for the Player's item and spark inventories
+		//be greedy and select all columns
+		//add a WHERE for this
+		Cursor databaseQuery = database.query(table, null, where, null, null, null, null);
+				
+		Log.d("SQL", "Game data cursor is " + Integer.toString(databaseQuery.getCount()) + " by "
+				+ Integer.toString(databaseQuery.getColumnCount()));
+				
+		//probably not needed
+		databaseQuery.moveToFirst();
+		
+		return databaseQuery;
+	}
+	
+	private Inventory readInventory(Cursor query){
+		Vector<Entity> newEntities = null;
+		Inventory newInventory = new Inventory();
+		
+		try{
+			for(int i = 0; i < query.getCount(); i++){
+				//grab the entities mapped by this row and cram 'em somewhere
+				newEntities = EntityCreationManager.getInstance().createEntity(
+						query, currentContext);
+				
+				//add entities to new inventory
+				for(int j = 0; j < newEntities.size(); j++){
+					newInventory.addEntity(newEntities.elementAt(j));
+				}
+				
+				query.moveToNext();
+			}
+		}
+		catch(TypeNotPresentException e){
+			Log.d("DEBUG", "Error, entity is not a spark or item");
+			//do something with the game state, since the player's inventory is clearly in
+			//an inconsistent state!
+		}
+		
+		return newInventory;
 	}
 }
--- src/com/sparkhunter/res/Player.java
+++ src/com/sparkhunter/res/Player.java
@@ -1,13 +1,6 @@
 package com.sparkhunter.res;
 
-import java.util.Vector;
-
-import android.R;
-import android.app.Activity;
 import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 public class Player {
 	//critical to make player a singleton, for cross-activity usage
@@ -20,61 +13,17 @@ public class Player {
 	public String playerName;
 	public String enemyID;
 	
-	private SQLiteDatabase gameData;
-	private Cursor dataQuery;
 	private Inventory itemInventory = new Inventory();
 	private Inventory sparkInventory = new Inventory();
 	
-	private static final String ITEM_TABLE = "player_data"; //TODO make this a R.strings reference
-	
 	private Player() {}
 	
+	//So much cleaner now that that damn SQL's abstracted away
 	public void initializeInventory(Context c) {
-		int quantity = 0;
+		GameDataManager manager = new GameDataManager(c);
 		
-		//create a database opener, and use it get a handle on the player database 
-		SQLGameDataOpener openHelper = new SQLGameDataOpener(c);
-		gameData = openHelper.open();
-		
-		//read in the data for the Player's item and spark inventories
-		//be greedy and select all columns
-		dataQuery = gameData.query(ITEM_TABLE, null, null, null, null, null, null);
-		
-		Log.d("SQL", "Game data cursor is " + Integer.toString(dataQuery.getCount()) + " by "
-				+ Integer.toString(dataQuery.getColumnCount()));
-		
-		//probably not needed
-		dataQuery.moveToFirst();
-		
-		//need to figure out what's a spark, and what's an item, and add it to the 
-		//correct inventory
-		try{
-			for(int i = 0; i < dataQuery.getCount(); i++){
-				Vector<Entity> entities = new Vector<Entity>();
-				
-				//grab the entities mapped by this row and cram 'em somewhere
-				entities = EntityCreationManager.getInstance().createEntity(dataQuery, c);
-				
-				//TODO add all needed getters to Entity interface
-				for(int j = 0; j < entities.size(); j++){
-					Log.d("DEBUG", "entity is " + entities.get(0).getType());
-					
-					if(entities.get(0).getType().equals("ITEM")){
-						itemInventory.addEntity(entities.get(j));
-					}
-					else{
-						sparkInventory.addEntity(entities.get(j));
-					}
-				}
-				
-				dataQuery.moveToNext();
-			}
-		}
-		catch(TypeNotPresentException e){
-			Log.d("DEBUG", "Error, entity is not a spark or item");
-			//do something with the game state, since the player's inventory is clearly in
-			//an inconsistent state!
-		}
+		itemInventory = manager.getAllPlayerItems();
+		sparkInventory = manager.getAllPlayerSparks();
 	}
 	
 	public static Player getInstance() {
